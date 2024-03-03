5fd9442321f9e76f865c9017596882e863169e4d
gooseman789 <106124000+gooseman789@users.noreply.github.com>
2024-03-03 16:07:34 -0500

    adding links for buttons within the cards

--- Portfolio/src/components/pages/Portfolio.jsx
+++ Portfolio/src/components/pages/Portfolio.jsx
@@ -5,6 +5,7 @@ import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 
+
 export default function Portfolio() {
     return (
         <Container>
@@ -12,20 +13,20 @@ export default function Portfolio() {
                 <Col>
                     <Card>
                         <Card.Title>Launch Pad</Card.Title>
-                        <Card.Img></Card.Img>
+                        <Card.Img src="../../assets/LaunchPad.png"></Card.Img>
                     </Card>
                 </Col>
                 <Col>
                     <Card>
                         <ButtonGroup>
-                            <Button>Deployed App</Button>
-                            <Button>GitHub</Button>
+                            <Button href="https://launch-pad-4e24e93f1929.herokuapp.com/">Deployed App</Button>
+                            <Button  href="https://github.com/andrewross4224/Launchpad">GitHub</Button>
                         </ButtonGroup>
                         <Card.Text></Card.Text>
                     </Card>
                 </Col>
             </Row>
-            <Row>
+            {/* <Row>
                 <Col>
                     <Card>
                         <Card.Title>Bookshelf</Card.Title>
@@ -35,25 +36,25 @@ export default function Portfolio() {
                 <Col>
                     <Card>
                         <ButtonGroup>
-                            <Button>Deployed App</Button>
-                            <Button>GitHub</Button>
+                            <Button href="https://obscure-brook-37683-7269a533acb8.herokuapp.com/profile">Deployed App</Button>
+                            <Button href="https://github.com/AFitzgerald95/Group-9-Project-2">GitHub</Button>
                         </ButtonGroup>
                         <Card.Text></Card.Text>
                     </Card>
                 </Col>
-            </Row>
+            </Row> */}
             <Row>
                 <Col>
                     <Card>
                         <Card.Title>My Anime List</Card.Title>
-                        <Card.Img></Card.Img>
+                        <Card.Img src="../../assets/AnimeList.png"></Card.Img>
                     </Card>
                 </Col>
                 <Col>
                     <Card>
                         <ButtonGroup>
-                            <Button>Deployed App</Button>
-                            <Button>GitHub</Button>
+                            <Button href="https://github.com/gooseman789/Project-1">Deployed App</Button>
+                            <Button href="https://gooseman789.github.io/Project-1/">GitHub</Button>
                         </ButtonGroup>
                         <Card.Text></Card.Text>
                     </Card>
@@ -63,13 +64,13 @@ export default function Portfolio() {
                 <Col>
                     <Card>
                         <Card.Title>E-commerce</Card.Title>
-                        <Card.Img></Card.Img>
+                        <Card.Img src="../../assets/ECommerce.png"></Card.Img>
                     </Card>
                 </Col>
                 <Col>
                     <Card>
                         <ButtonGroup>
-                            <Button>GitHub</Button>
+                            <Button href="https://github.com/gooseman789/E-commerce">GitHub</Button>
                         </ButtonGroup>
                         <Card.Text></Card.Text>
                     </Card>
@@ -79,13 +80,13 @@ export default function Portfolio() {
                 <Col>
                     <Card>
                         <Card.Title>Social Network</Card.Title>
-                        <Card.Img></Card.Img>
+                        <Card.Img src="../../assets/SocialNetwork.png"></Card.Img>
                     </Card>
                 </Col>
                 <Col>
                     <Card>
                         <ButtonGroup>
-                            <Button>GitHub</Button>
+                            <Button href="https://github.com/gooseman789/Social-Network">GitHub</Button>
                         </ButtonGroup>
                         <Card.Text></Card.Text>
                     </Card>
@@ -95,14 +96,14 @@ export default function Portfolio() {
                 <Col>
                     <Card>
                         <Card.Title>Quiz Game</Card.Title>
-                        <Card.Img></Card.Img>
+                        <Card.Img src="../../assets/Quiz.png"></Card.Img>
                     </Card>
                 </Col>
                 <Col>
                     <Card>
                         <ButtonGroup>
-                            <Button>Deployed App</Button>
-                            <Button>GitHub</Button>
+                            <Button href="https://gooseman789.github.io/Quiz/">Deployed App</Button>
+                            <Button href="https://github.com/gooseman789/Quiz">GitHub</Button>
                         </ButtonGroup>
                         <Card.Text></Card.Text>
                     </Card>
